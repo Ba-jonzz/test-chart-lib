331126075f2588a71bf5eab5b80f9d65136b7dd4
Ba-jonzz <bahaj@hello-jonzz.com>
2024-06-28 15:38:31 +0100

    first commit

---
+++ src/TestChart.js
@@ -0,0 +1,31 @@
+import Chart from 'library-chart';
+
+const TestCart = () => {
+
+const data = [
+    {name: "100", value: 60, category: 'bobi'},
+    {name: "120", value: 20, category: 'bobi'},
+    {name: "150", value: 12, category: 'bobi'},
+    {name: "200", value: 24, category: 'bobi'},
+    {name: "100", value: 33, category: 'paul'},
+    {name: "120", value: 56, category: 'paul'},
+    {name: "150", value: 13, category: 'paul'},
+    {name: "200", value: 60, category: 'paul'},
+    {name: "100", value: 12, category: 'bernadette'},
+    {name: "120", value: 24, category: 'bernadette'},
+    {name: "150", value: 6, category: 'bernadette'},
+    {name: "200", value: 30, category: 'bernadette'},
+  ]
+  
+  const charts = [
+      {
+        type: 'lines',
+        categories: ['bernadette', 'paul']
+      }
+    ]
+  return (
+    <Chart data={data} charts={charts}></Chart>
+  )
+}
+
+export default TestCart
